67c4e949160ae685bd33af08701467f90a14d489
NewDeVGUY60 <[guynicolas60@gmail.com](mailto:guynicolas60@gmail.com)>
2023-01-25 15:20:46 +0100

    add style on page2

--- src/app/page-selection-characters/page-selection-characters.component.ts
+++ src/app/page-selection-characters/page-selection-characters.component.ts
@@ -1,31 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HeroesInformationsService } from 'src/app/heroes-informations.service'
-
+import { HeroesInformationsService } from 'src/app/heroes-informations.service';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-page-selection-characters',
   templateUrl: './page-selection-characters.component.html',
   styleUrls: ['./page-selection-characters.component.scss']
 })
-export class PageSelectionCharactersComponent  {
-  constructor(private router: Router , private heroes: HeroesInformationsService) {
-  
-  }
-  
+export class PageSelectionCharactersComponent {
+  constructor(private router: Router, private heroes: HeroesInformationsService, private data: DataService) { }
 
-
-
+  title = this.data.title
 
   page3(): void {
     this.router.navigate(["/recap"]);
   }
 
+  page2(): void {
+    this.router.navigate(["/recap"]);
+  }
 
-
-page2():void{
-  this.router.navigate(["/recap"]);
-}
 
 
 }
